cc3cad4b058683a2f788d682ddc820202a8e8516
dannamayac <dxmayac@uqvirtual.edu.co>
2024-03-19 10:07:18 -0500

    Cambios en home y homestyles

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserTie } from '@fortawesome/free-solid-svg-icons'
+import { faFileInvoice } from '@fortawesome/free-solid-svg-icons'
+import { faFileCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import { faFileCircleCheck } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faGears } from '@fortawesome/free-solid-svg-icons'
 import '../styles/HomeStyles.css'
 import Sidebar from '../components/SideBar';
 import Header from '../components/Header';
@@ -14,32 +21,62 @@ const Home = () =>{
                 <div className="left-in">
                     <div className="button-container">
                         <button className="custom-button gestion">
-                            <span className="button-text">Gestión y Administración</span>
-                            <div className="sub-button">Gestionar &gt;</div>
+                            <div className="left-bu">
+                                <span className="button-text">Gestión y Administración</span>
+                                <div className="sub-button">Gestionar &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&gt;</div>
+                            </div>
+                            <div className="right-bu">
+                                <FontAwesomeIcon icon={faUserTie} />
+                            </div>
                         </button>
                         <button className="custom-button facturacion">
-                            Facturación
-                            <div className="sub-button">Gestionar     &gt;</div>
+                            <div className="left-bu">
+                                Facturación
+                                <div className="sub-button">Gestionar &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&gt;</div>
+                            </div>
+                            <div className="right-bu">
+                                <FontAwesomeIcon icon={faFileInvoice} />
+                            </div>
                             </button>
                     </div>
                 </div>
                 <div className="right-in">
                     <div className="button-container">
                         <button className="custom-button nueva-venta">
-                            Nueva Venta (Nuevo préstamo)
-                            <div className="sub-button">Gestionar     &gt;</div>
-                            </button>
+                            <div className="left-bu">
+                                Nueva Venta (Nuevo préstamo)
+                                <div className="sub-button">Gestionar &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&gt;</div>
+                            </div>
+                            <div className="right-bu">
+                                <FontAwesomeIcon icon={faFileCirclePlus} />
+                            </div>
+                        </button>
                         <button className="custom-button aprobaciones">
-                            Aprobaciones
-                            <div className="sub-button">Gestionar     &gt;</div>
+                            <div className="left-bu">
+                                Aprobaciones
+                                <div className="sub-button">Gestionar &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&gt;</div>
+                            </div>
+                            <div className="right-bu">
+                                <FontAwesomeIcon icon={faFileCircleCheck} />
+                            </div>
                             </button>
                         <button className="custom-button mapa">
+                        <div className="left-bu">
                             Mapa
-                            <div className="sub-button">Gestionar     &gt;</div>
+                            <div className="sub-button">Gestionar &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&gt;</div>
+                            </div>
+                            <div className="right-bu">
+                            <FontAwesomeIcon icon={faLocationDot} />
+                            </div>
                             </button>
                         <button className="custom-button configuracion">
+                        <div className="left-bu">
                             Configuración
-                            <div className="sub-button">Gestionar     &gt;</div>
+                            <div className="sub-button">Gestionar &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&gt;</div>
+                            </div>
+                            <div className="right-bu">
+                            <FontAwesomeIcon icon={faGears} />
+                            </div>
                             </button>
                     </div>
                 </div>
